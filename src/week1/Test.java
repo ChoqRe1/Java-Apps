b1e3ffe104dc51e34a4b7ec13520a0a7d443e2dc
Rémi Choquette <remichoq@gmail.com>
2016-09-23 12:14:42 -0400

    Adding current files
    I am adding the files I currently have for this project :)

---
+++ src/week1/Test.java
@@ -0,0 +1,12 @@
+package week1;
+import java.util.Scanner;
+/**
+ * Created by Remi on 2016-09-23.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        
+    }
+}
