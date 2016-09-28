f292380b255e49583fda8a873a8de158cf051a53
Rémi Choquette <remichoq@gmail.com>
2016-09-28 16:53:44 -0400

    Finished
    Created get description and finished full project + adding new project
    clock

--- Oop problems/PizzaProblem/src/pizzaproblem/Pizza.java
+++ Oop problems/PizzaProblem/src/pizzaproblem/Pizza.java
@@ -1,5 +1,5 @@
 package pizzaproblem;
-
+//Created by ChoqRe1
 public class Pizza {
     private String size;
     private int cheese;
@@ -37,7 +37,7 @@ public int getCheese()
 {
     return cheese;
 }
-public int getPeperonni()
+public int getPepperoni()
 {
     return pepperoni;
 }
@@ -45,20 +45,30 @@ public int getHam()
 {
     return ham;
 }
+
+public String getDescription(){
+        String description;
+    description= "This is your pizza's information:\n" + "Size: " + getSize() +"\nToppings:\n" + "Cheese: " + getCheese() + "\n" + "Pepperoni: " + getPepperoni() + "\n" + "Ham: " + getHam() + "\n"; 
+    return description;
+}
+
 public double calcCost(){
     if( null != size)switch (size) {
             case "Small":
-                cost = +10;
+                cost += 10;
                 break;
             case "Medium":
-                cost = +12;
+                cost += 12;
                 break;
             case "Large":
-                cost = +14;
+                cost += 14;
                 break;
             default:
                 break;
         }
+    cost += (2*cheese);
+    cost += (2*pepperoni);
+    cost += (2*ham);
     return cost;
 }
 }
