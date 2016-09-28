e7d48109d7c522e863f4b3ed4820bdaf5efba9a8
Rémi Choquette <remichoq@gmail.com>
2016-09-28 13:26:50 -0400

    Pizza
    added the pizza creation project

---
+++ Oop problems/PizzaProblem/src/pizzaproblem/Pizza.java
@@ -0,0 +1,43 @@
+package pizzaproblem;
+
+public class Pizza {
+private ;
+private int cheese;
+private int pepperoni;
+private int ham;
+
+public Pizza(S s, int c, int p, int h)
+{
+    e=e;
+    cheese=c;
+    pepperoni=p;
+    ham=h;
+}
+public void;
+public void setCheese(int c)
+{
+    cheese=c;
+}
+public void setPepperoni(int p)
+{
+    pepperoni=p;
+}public void setHam(int h)
+{
+    ham=h;
+}
+public ;
+public int getCheese()
+{
+    return cheese;
+}
+public int getPeperonni()
+{
+    return pepperoni;
+}
+public int getHam()
+{
+    return ham;
+}
+
+
+}
---
+++ Oop problems/PizzaProblem/src/pizzaproblem/PizzaProblem.java
@@ -0,0 +1,9 @@
+package pizzaproblem;
+
+public class PizzaProblem {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
