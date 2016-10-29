371cbca3de6cf8a4e7910f929308177663276a29
Rémi Choquette <remichoq@gmail.com>
2016-10-29 13:04:42 -0400

    Updated Distance Travelled

--- src/Chapter5/distanceTraveled/DistanceTraveled.java
+++ src/Chapter5/distanceTraveled/DistanceTraveled.java
@@ -1,26 +1,30 @@
 package distanceTraveled;
 import java.util.Scanner;
+import java.io.*;
 
 /**
  * Chapter5.distanceTraveled created by Remi on 2016-10-28.
  */
 public class DistanceTraveled {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         //Calling Variables
         int hours;
         int speed;
-        //Creating Scanner
+        //Creating Scanner & File
         Scanner input=new Scanner(System.in);
+        File file=new File("itinirary.txt");
+        PrintWriter pw=new PrintWriter(file);
         //User Interaction
         System.out.println("How many hours will you travel for?");
         hours=input.nextInt();
         System.out.println("What is the speed you will be travelling at?");
         speed=input.nextInt();
         //Creating Chart
-        System.out.println("HOURS       DISTANCE");
+        pw.println("HOURS       DISTANCE");
         //for Loop
         for(int i=1; i<=hours; i++){
-            System.out.println("   "+(i)+"        "+(speed*i));
+            pw.println("   "+(i)+"        "+(speed*i));
         }
+        pw.close();
     }
 }
