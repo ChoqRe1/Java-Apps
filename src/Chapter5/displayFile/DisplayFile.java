ef342632703f96e67b6fda61e4073c53130a4766
Rémi Choquette <remichoq@gmail.com>
2016-11-09 17:57:23 -0500

    Chapter 5 #14

---
+++ src/Chapter5/displayFile/DisplayFile.java
@@ -0,0 +1,17 @@
+package Chapter5.displayFile;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Chapter5.displayFile created by Remi on 2016-11-09.
+ */
+public class DisplayFile {
+    public static void main(String[] args) throws IOException{
+
+        FileDisplay df=new FileDisplay("data.txt");
+
+//        df.displayHead();
+//        df.displayContents();
+        df.displayWithLineNumbers();
+    }
+}
---
+++ src/Chapter5/displayFile/FileDisplay.java
@@ -0,0 +1,44 @@
+package Chapter5.displayFile;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Chapter5.displayFile created by Remi on 2016-11-09.
+ */
+public class FileDisplay {
+    private File file;
+    private String f;
+    private Scanner input;
+
+    public FileDisplay(String f) throws IOException{
+        file = new File(f);
+        this.f = f;
+        input=new Scanner(file);
+    }
+
+    public void displayHead(){
+        int count=0;
+        while(input.hasNext()&&count<=5){
+            String line = input.nextLine();
+            System.out.println(line);
+            count++;
+        }
+    }
+
+    public void displayContents(){
+        while(input.hasNext()){
+            String line = input.nextLine();
+            System.out.println(line);
+        }
+    }
+
+    public void displayWithLineNumbers(){
+        int count=1;
+        while(input.hasNext()){
+            String line = input.nextLine();
+            System.out.println(count+": "+line);
+            count++;
+        }
+    }
+
+}
